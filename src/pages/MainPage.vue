0272ca4add57a560c3f0c41a4379fa9c4ef2c010
김성은 <kse012050@naver.com>
2023-11-04 07:08:10 +0900

    솔루션 영역 임시 완료

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,36 +1,42 @@
 <template>
     <section class="mainPage">
         <div class="topArea" data-full>
-            <h2>
-                <b>TWh</b>
-                테라와트아워
-            </h2>
-            <p>재생에너지 전력거래, 지속 가능한 내일을 만듭니다.</p>
+            <div>
+                <h2>
+                    <b>TWh</b>
+                    테라와트아워
+                </h2>
+                <p class="cursor">재생에너지 전력거래, 지속 가능한 내일을 만듭니다.</p>
+            </div>
             <div class="bg"></div>
         </div>
         <div class="solutionArea" data-full>
             <ul>
                 <li>
-                    <strong>
-                        기업을 위한<br>
-                        RE100 솔루션
-                    </strong>
-                    <p>
-                        복잡한 RE100 이행,<br>
-                        테라와트아워로 쉬워집니다.
-                    </p>
-                    <a href="#" class="arrow-white">자세히 보기<span></span></a>
+                    <div>
+                        <strong>
+                            기업을 위한<br>
+                            RE100 솔루션
+                        </strong>
+                        <p>
+                            복잡한 RE100 이행,<br>
+                            테라와트아워로 쉬워집니다.
+                        </p>
+                        <a href="#" class="arrow-white">자세히 보기<span></span></a>
+                    </div>
                 </li>
                 <li>
-                    <strong>
-                        발전사업자를 위한<br>
-                        수익 최적화 솔루션
-                    </strong>
-                    <p>
-                        업계 최고 수준의<br>
-                        발전소 매입 조건을 제안합니다.
-                    </p>
-                    <a href="#" class="arrow-white">자세히 보기<span></span></a>
+                    <div>
+                        <strong>
+                            발전사업자를 위한<br>
+                            수익 최적화 솔루션
+                        </strong>
+                        <p>
+                            업계 최고 수준의<br>
+                            발전소 매입 조건을 제안합니다.
+                        </p>
+                        <a href="#" class="arrow-white">자세히 보기<span></span></a>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -95,30 +101,122 @@ export default {
     data() {
         return {
             mainPage: undefined,
-            fullSelectors: undefined,
+            // fullSelectors: undefined,
         }
     },
     methods: {
         fullPageStyle() {
             this.mainPage = document.querySelector('.mainPage');
             this.fullSelectors = document.querySelectorAll('[data-full]');
-            // const element = this.$refs;
-            // console.log(element);
-            // console.log(this.$refs.full);
-            console.log(this.fullSelectors);
             let vh = window.innerHeight * 0.01;
             this.mainPage.style.setProperty('--vh', `${vh}px`)
-            // this.fullSelectors.forEach(function(element) {
-            //     element.style.setProperty('--vh', `${vh}px`)
-            // })
-        }
+        },
+        typingEvent() {
+            const typingEle = document.querySelector('.topArea p');
+            // const typginBackup = typingEle.innerHTML;
+            typingEle.innerHTML = ''
+            this.fullEvent();
+            /* let count = 0;
+            setTimeout(()=>{
+                const typingAni = setInterval(() => {
+                    typingEle.innerHTML += typginBackup[count];
+                    count++
+                    if(!typginBackup[count]){
+                        clearInterval(typingAni);
+                        setTimeout(()=>{
+                            typingEle.classList.remove('cursor');
+                            document.querySelector('.topArea').classList.add('intro');
+                            document.querySelector('.mainPage .topArea').addEventListener('animationend',()=>{
+                                console.log('끝1');
+                                this.fullEvent();
+                            })
+                            document.querySelector('.mainPage .topArea.intro > .bg').addEventListener('animationend',function(){
+                                console.log('끝2');
+                            })
+                        }, 1000)
+                    }
+                }, 100);
+            },1000); */
+        },
+        fullEvent(){
+            const fullSelectors = document.querySelectorAll('[data-full]');
+            let isFull = false;
+            console.log(fullSelectors);
+            fullSelectors.forEach((element, idx)=>{
+                let currentIdx = idx;
+                element.addEventListener('mousewheel', function(e){
+                    if(isFull){return}
+                    if(e.wheelDelta < 0 && fullSelectors[currentIdx + 1]){
+                        // currentIdx++;
+                        fullSelectors[idx + 1].classList.add('active')
+                        isFull = true;
+                    }else if(e.wheelDelta > 0 && fullSelectors[currentIdx - 1]){
+                        // currentIdx--;
+                        fullSelectors[currentIdx].classList.remove('active')
+                        isFull = true;
+                    }
+                    // console.log(fullSelectors[currentIdx]);
+                    if(isFull){
+                        // console.log(currentIdx);
+                        // fullSelectors[currentIdx].classList.contains('active') ? 
+                        //     fullSelectors[currentIdx + 1].classList.remove('active') :
+                        //     fullSelectors[currentIdx].classList.add('active');
+                        setTimeout(()=>{
+                            isFull = false;
+                        },1000)
+                    }
+                    // console.log(this);
+                    // console.log(idx);
+                })
+
+            })
+        },
+        
     },
     mounted() {
         this.fullPageStyle();
+        this.typingEvent();
+        // this.fullEvent();
         window.addEventListener('resize',this.fullPageStyle)
     }
 }
 </script>
 <style>
-    
+    body{overflow: hidden;}
+    [data-full]{position: fixed; left: 0; top: 0; width: 100%; z-index: -1; transition: 0s 1s z-index;}
+    [data-full].active{z-index: 1; transition: 0s;}
+    .mainPage .topArea{z-index: 1;}
+
+    /* header{opacity: 0; pointer-events: none;}
+    .mainPage .topArea{--color: #222; color: var(--color); position: relative; z-index: 1; background-color: white;}
+    .mainPage .topArea::after{content: ''; position: absolute; top: 50%; left: 0; width: 100%; height: 1px; transform: translateY(-50%); background: red;}
+    .mainPage .topArea > div{transform: translateY(calc(-50% + var(--typingHeight) / 2));}
+    .mainPage .topArea > div h2{opacity: 0;}
+    .mainPage .topArea > div p.cursor::after{content: ''; position: absolute; right: -0.2em; top: 0; width: 2px; height: 100%; background-color: var(--color);
+    animation: cursor 1s steps(2) infinite;}
+    .mainPage .topArea > .bg{clip-path: circle(0% at 50% 50%);}
+
+    @keyframes cursor {
+        0%{opacity: 0;}
+        100%{opacity: 1;}
+    }
+
+    header:has(+ .mainPage .topArea.intro){opacity: 1; transition: 1s 2.5s opacity; pointer-events: all;}
+    .mainPage .topArea.intro{--color: white; transition: 1s 2s color;}
+    .mainPage .topArea.intro > div{transform: translateY(0); transition: 1s transform;}
+    .mainPage .topArea.intro > div h2{opacity: 1; transition: 1s 0.8s opacity;}
+    .mainPage .topArea.intro > .bg{animation: topBG 3s 1.3s forwards;}
+    @keyframes topBG {
+        0%{clip-path: circle(0% at 50% 50%); opacity: 0;}
+        100%{clip-path: circle(100% at 50% 50%); opacity: 1;}
+    } */
+
+    .mainPage .solutionArea ul li{transform: translateY(0); transition: 0.5s transform;}
+    .mainPage .solutionArea ul li:nth-child(1){transform: translateY(-100%);}
+    .mainPage .solutionArea ul li:nth-child(2){transform: translateY(100%);}
+    .mainPage .solutionArea ul li div{opacity: 0; transform: translateY(100px);}
+    .mainPage .solutionArea.active ul li{transform: translateY(0); transition: 0.5s transform;}
+    .mainPage .solutionArea.active ul li div{opacity: 1; transform: translateY(0); transition-property: opacity, transform; transition-duration: 0.5s; transition-delay: 0.3s; }
+    .mainPage .terraArea{transform: translateX(100%); background-color: rgba(0, 15, 53, 0.9); transition: 0.5s transform;}
+    .mainPage .terraArea.active{transform: translateX(0); }
 </style>
