9009f455dcc6a6c9d269a50f32f896bd85919977
김성은 <kse012050@naver.com>
2023-11-06 04:03:06 +0900

    PC - 사업 문의 페이지 완료

--- src/pages/InquiryPage.vue
+++ src/pages/InquiryPage.vue
@@ -1,7 +1,40 @@
 <template>
-    <div>
-        사업 문의
-    </div>
+    <section class="inquiryPage contentSize">
+        <h2>사업 문의</h2>
+        <form>
+            <fieldset>
+                <ul>
+                    <li>
+                        <label for="">업체명*</label>
+                        <input type="text" placeholder="업체명을 작성하세요. (2자 - 20자)">
+                    </li>
+                    <li>
+                        <label for="">담당자(성함)*</label>
+                        <input type="text" placeholder="담당자 성함을 작성하세요. (2자 - 20자)">
+                    </li>
+                    <li>
+                        <label for="">연락처*</label>
+                        <input type="text" placeholder="휴대폰번호나 내선번호를 작성하세요. (8자 이상, ‘-‘ 제외)">
+                    </li>
+                    <li>
+                        <label for="">이메일*</label>
+                        <input type="text" placeholder="이메일을 형식에 맞게 작성하세요.">
+                    </li>
+                    <li>
+                        <label for="">상세 내용</label>
+                        <textarea placeholder="기타 문의 및 참고 내용을 작성하세요. (500자 내)"></textarea>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="" checked>
+                        <label for=""><b>개인정보이용방침</b>에 동의합니다.</label>
+                        <input type="checkbox" name="" id="">
+                        <label for="">마케팅 정보 수신에 동의합니다.</label>
+                    </li>
+                </ul>
+                <input type="submit" value="문의하기" class="btn-black">
+            </fieldset>
+        </form>
+    </section>
 </template>
 <script>
 export default {
