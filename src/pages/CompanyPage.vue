35279faa544d3a8fa3300ea8d0f817999c6a05ad
김성은 <kse012050@naver.com>
2023-11-07 23:39:36 +0900

    PC - 기업 소개 페이지 완료

--- src/pages/CompanyPage.vue
+++ src/pages/CompanyPage.vue
@@ -26,8 +26,76 @@
                 <p>분산에너지 중심의 에너지 패러다임 전환을 선도하겠습니다.</p>
             </div>
         </div>
-        <div class="peopleArea">
-            <strong></strong>
+        <div class="peopleArea contentSize-padding">
+            <strong>
+                재생에너지, 금융 분야<br>
+                최고의 전문가가 함께합니다.
+            </strong>
+            <ul>
+                <li>
+                    <img src="../images/company-people01.png" alt="고성훈 이미지">
+                    <b>고성훈</b>
+                    <span>Ko Sung Hoon</span>
+                    <small>CEO</small>
+                    <p>한화컨버전스 에너지플랫폼사업 담당 </p>
+                    <p>한화에너지 에너지솔루션 Lab 센터장 </p>
+                    <p>한화시스템 전략기획팀장 </p>
+                    <p>LGCNS 스마트시티 사업개발</p>
+                </li>
+                <li>
+                    <img src="../images/company-people02.png" alt="차성종 이미지">
+                    <b>차성종</b>
+                    <span>Cha Seong Jong</span>
+                    <small>CFO</small>
+                    <p>신한은행 호남본부장 </p>
+                    <p>신한은행 광주기업금융센터장 </p>
+                    <p>신한은행 목포금융센터장 </p>
+                    <p>신한은행 순천연향동지점장</p>
+                </li>
+                <li>
+                    <img src="../images/company-people03.png" alt="김영근 이미지">
+                    <b>김영근</b>
+                    <span>Kim Young Keun</span>
+                    <small>전력거래사업팀장</small>
+                    <p>한화컨버전스 에너지플랫폼사업팀</p>
+                    <p>한화임팩트 IPP사업팀</p>
+                </li>
+                <li>
+                    <img src="../images/company-people04.png" alt="정윤지 이미지">
+                    <b>정윤지</b>
+                    <span>Jung Yun Ji</span>
+                    <small>경영지원팀장</small>
+                    <p>삼일회계법인 Deal (Infra/RE본부)</p>
+                    <p>삼일회계법인 감사 (금융 2본부)</p>
+                </li>
+            </ul>
+        </div>
+        <p class="phraseArea contentSize-padding">
+            에너지를 생산하는 발전사업자와<br>
+            소비하는 기업을 연결하는<br>
+            <b>국내 최대의 전력거래 플랫폼</b>으로<br>
+            자리매김 하겠습니다.
+        </p>
+        <div class="VPPArea contentSize-padding">
+            <b>통합발전소(VPP)</b>
+            <div>
+                <img src="../images/company-VVPImg.png" alt="통합발전소 VPP 이미지">
+            </div>
+        </div>
+        <hr>
+        <div class="historyArea contentSize-padding">
+            <strong>연혁</strong>
+            <ol title="2023">
+                <li><small>09</small>한화 신한 테라와트아워 설립</li>
+                <li><small>04</small>신한자산운용, 한화컨버전스 재생에너지 전력거래사업 전문 합작법인 설립 협약 체결</li>
+            </ol>
+        </div>
+        <div class="bottomArea contentSize-padding">
+            <img src="../images/logo-white.png" alt="">
+            <div>
+                <a href="" class="btn-download"><b>CI</b>(AI)</a>
+                <a href="" class="btn-download"><b>PR</b>(PDF)</a>
+            </div>
         </div>
     </section>
 </template>
