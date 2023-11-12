492c8cfa4580ffafc21c9658eced2a2bf385f9c3
김성은 <kse012050@naver.com>
2023-11-13 06:12:42 +0900

    관리자 - 사업 문의 상세 페이지 완료

--- src/pages/admin/InquiryDetail.vue
+++ src/pages/admin/InquiryDetail.vue
@@ -1,7 +1,128 @@
-<template lang="">
-    <div>
-        사업 관리 상세
-    </div>
+<template>
+    <section class="inquiryDetailPage contentSize">
+        <h2><a href="">No.9999</a></h2>
+        <div>
+            <ul class="infoArea">
+                <li>
+                    <b class="required">성함</b>
+                    <div>
+                        <p>홍길동</p>
+                    </div>
+                </li>
+                <li>
+                    <b>회사</b>
+                    <div>
+                        <p>테슬라</p>
+                    </div>
+                </li>
+                <li>
+                    <b class="required">연락처</b>
+                    <div>
+                        <p>01055558888</p>
+                    </div>
+                </li>
+                <li>
+                    <b class="required">이메일</b>
+                    <div>
+                        <p>asdfasdfasdf@tesla.com</p>
+                    </div>
+                </li>
+                <li>
+                    <b class="required">상담목적</b>
+                    <div>
+                        <p>RE100 이행솔루션</p>
+                    </div>
+                </li>
+                <li>
+                    <b class="required">RE100 이행목표</b>
+                    <div>
+                        <p>전력사용량 대비 *% 재생에너지 조달</p>
+                    </div>
+                </li>
+                <li>
+                    <b>연간 전력사용량</b>
+                    <div>
+                        <p>20,000GWh</p>
+                    </div>
+                </li>
+                <li>
+                    <b class="required">상담목적</b>
+                    <div>
+                        <p>PPA, REC 구매</p>
+                    </div>
+                </li>
+                <li>
+                    <b>상세 내용</b>
+                    <div>
+                        <p>
+                            저희는 어떤 회사이고 <br>
+                            한화 신한 테라와트아워와 협업하고 싶습니다. <br>
+                            비즈니스 계약 조건을 상세하게 확인할 수 있을까요?
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <b>동의</b>
+                    <div>
+                        <dl>
+                            <dt>개인정보 이용방침</dt>
+                            <dd>동의</dd>
+                        </dl>
+                        <dl>
+                            <dt>마케팅 정보 수신</dt>
+                            <dd>동의</dd>
+                        </dl>
+                    </div>
+                </li>
+            </ul>
+            <div class="answerArea">
+                <form>
+                    <fieldset>
+                        <ul>
+                            <li>
+                                <label for="">회신 여부</label>
+                                <div>
+                                    <select name="" id="">
+                                        <option value="">회신완료</option>
+                                    </select>
+                                </div>
+                            </li>
+                            <li>
+                                <label for="">비고</label>
+                                <div>
+                                    <input type="text" name="" id="" placeholder="작성자명">
+                                    <textarea name="" id="" placeholder="메모를 입력하세요. (최대 100자)"></textarea>
+                                </div>
+                            </li>
+                        </ul>
+                        <input type="submit" value="저장" class="btn-black">
+                        <div class="content-btn">
+                            <button class="btn-border">삭제</button>
+                            <input type="submit" value="수정" class="btn-black">
+                        </div>
+                    </fieldset>
+                </form>
+                <ul>
+                    <li>
+                        <b>박와트</b>
+                        <div>
+                            <p>유선 문의가 와서 김테라 매니저님 연결했습니다.</p>
+                            <time>2023.11.02 12:34:56</time>
+                        </div>
+                    </li>
+                    <li>
+                        <b>박와트</b>
+                        <div>
+                            <p>유선 문의가 와서 김테라 매니저님 연결했습니다. 유선 문의가 와서 김테라 매니저님 연결했습니다.유선 문의가 와서 김테라 매니저님 연결했습니다.</p>
+                            <time>2023.11.02 12:34:56</time>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+
+    </section>
 </template>
 <script>
 export default {
