14243ccd27be0584bf34cd0cfb4555c217ce79f8
김성은 <kse012050@naver.com>
2023-11-27 05:55:04 +0900

    파비콘, 관리자 로그아웃

---
+++ src/components/modal/ModalLogout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="content-modal">
+        <div class="logoutArea">
+            <strong>알림</strong>
+            <p>로그아웃 되었습니다.</p>
+            <div class="btnArea">
+                <button class="black" @click.prevent="this.$router.push({path: '/admin'})">확인</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ModalLogout',
+    props: {
+        isModal: Boolean,
+        modalText: Object
+    },
+}
+</script>
+<style>
+    
+</style>
--- src/pages/admin/AdminRoot.vue
+++ src/pages/admin/AdminRoot.vue
@@ -8,22 +8,37 @@
                     <li><router-link to="/admins/board">게시판 관리</router-link></li>
                     <li><router-link to="/admins/inquiry">사업 문의 관리</router-link></li>
                 </ul>
+                <button @click="logout">로그아웃</button>
             </div>
         </header>
+        <modal-logout v-if="isLogout"/>
         <router-view></router-view>
     </div>
 </template>
 <script>
 import { globalStyle } from '../../css/style.js'
+import ModalLogout from '@/components/modal/ModalLogout.vue'
 
 export default {
     name: 'AdminRoot',
+    components: { ModalLogout },
+    data(){
+        return{
+            isLogout: false
+        }
+    },
+    methods: {
+        logout(){
+            this.isLogout = true
+            sessionStorage.removeItem('adminName')
+            sessionStorage.removeItem('token')
+        }
+    },
     mounted() {
         !sessionStorage.getItem('token') && this.$router.push({path: '/admin'})
         globalStyle();
     },
     updated(){
-        console.log('이건?');
         globalStyle();
     }
 }
